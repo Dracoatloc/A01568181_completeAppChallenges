cf636a10c3c109cf15244f1f6c097cc50a07a71b
user192468 <user192468@telehouse-moboware.dmarc.si1.atlanticmetro.net>
2021-05-07 20:32:55 -0400

    Challenge 1

---
+++ BookStore/Book.swift
@@ -0,0 +1,18 @@
+//
+//  Book.swift
+//  BookStore
+//
+//  Created by user192468 on 3/5/21.
+//
+
+import Foundation
+
+class Book {
+    var title: String = ""
+    var author: String = ""
+    var description: String = ""
+    var genre: String = ""
+    var pages: Int = 0
+    var readThisBook: Bool = false	
+
+}
---
+++ BookStore/MasterViewController.swift
@@ -0,0 +1,81 @@
+//
+//  MasterViewController.swift
+//  BookStore
+//
+//  Created by user192468 on 3/9/21.
+//
+
+import UIKit
+
+class MasterViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, BookStoreDelegate
+{
+    
+    // MARK: Properties
+        var detailViewController : DetailViewController? = nil
+        var myBookStore = BookStore()
+
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: Table View
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myBookStore.bookList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel!.text = myBookStore.bookList[indexPath.row].title
+        cell.accessoryType = .disclosureIndicator
+        return cell
+
+    }
+    
+    // MARK: Segues
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showDetail" {
+            if let indexPath = tableView.indexPathForSelectedRow {
+                let selectedBook : Book = myBookStore.bookList[indexPath.row]
+                let controller = (segue.destination as! DetailViewController)
+                controller.detailItem = selectedBook
+                controller.delegate = self
+            }
+        } else if segue.identifier == "addBookSegue" {
+            let controller = (segue.destination as! AddViewController)
+            controller.delegate = self
+        }
+    }
+    
+    func newBook(_ controller: AnyObject, newBook: Book) {
+        myBookStore.bookList.append(newBook)
+        tableView.reloadData()
+        navigationController?.popToRootViewController(animated: true)
+    }
+
+    
+    func editBook(_ controller: AnyObject, editBook: Book) {
+        if let row = tableView.indexPathForSelectedRow?.row {
+            myBookStore.bookList[row] = editBook
+        }
+        
+        tableView.reloadData()
+        navigationController?.popToRootViewController(animated: true)
+    }
+    
+    func deleteBook(_ controller: AnyObject) {
+        if let row = tableView.indexPathForSelectedRow?.row {
+            myBookStore.bookList.remove(at: row)
+        }
+        
+        tableView.reloadData()
+        navigationController?.popToRootViewController(animated: true)
+    }
+}
+
